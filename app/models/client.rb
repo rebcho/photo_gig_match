149549ad9e5f3ccd5f5b2c09d86ec07a9d86d636
Raghu Betina <raghu@firstdraft.com>
2018-02-20 22:27:50 +0000

    Direct association between clients and gigs

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,9 @@
 class Client < ApplicationRecord
   # Direct associations
 
+  has_many   :gigs,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,6 +1,8 @@
 class Gig < ApplicationRecord
   # Direct associations
 
+  belongs_to :client
+
   # Indirect associations
 
   # Validations
