6d3b0e102b18b4eaff6677e1787257c045105e84
Raghu Betina <raghu@firstdraft.com>
2018-02-20 22:27:27 +0000

    Generate clients resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :clients
+  # Routes for the Client resource:
+  # READ
+  get "/clients", :controller => "clients", :action => "index"
+  get "/clients/:id", :controller => "clients", :action => "show"
+
+
   devise_for :photographers
   # Routes for the Photographer resource:
   # READ
